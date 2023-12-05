f1ceeba107b15e76a73ee72ca28da92dcd8b6106
spderman3333 <stefan.v.dikov@gmail.com>
2023-12-05 19:04:46 +0000

    removed webhook items

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import json
 import os
 import requests
-from posthook import Posthook as ph
 
 
 r = requests.get("https://drgapi.com/v1/deepdives")
-web_url = os.environ['URL_VAL']
-message = ""
 
 # print(r.status_code)
 # print(r.text)
@@ -31,20 +28,30 @@ dive_type = variants[0]["type"]
 dive_codename = variants[0]["name"]
 dive_biome = variants[0]["biome"]
 dive_seed = variants[0]["seed"]
+stages = variants[0]["stages"]
 stage_1 = variants[0]["stages"][0]
 stage_2 = variants[0]["stages"][1]
 stage_3 = variants[0]["stages"][2]
 
-mydict = {}
 
-print(dive_type)
-print(dive_codename)
-print(dive_biome)
-print(dive_seed)
+# print(dive_type)
+# print(dive_codename)
+# print(dive_biome)
+# print(dive_seed)
 
-for key, val in stage_1.items():
-  print(str(key) + " : " +str(val))
-  
+list = []
+for i in range(3):
+  # print(stages[i])
+  list.append(stages[i])
+print(list)
 
-message = ""
-# ph(web_url, message)
+
+
+# message = f"""{dive_type} | {dive_codename} | {dive_biome} | {dive_seed}
+# Dive Stages:
+# Stage 1:
+# Primary: {stage_1["primary"]}
+# Secondary: {stage_1["secondary"]}
+# Anomaly: {stage_1["anomaly"]}
+# Warning: {stage_1["warning"]}"""
+# print(message)
