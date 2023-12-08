093cb317a95595ffa080a3d814b6ef167235f4ec
spderman3333 <stefan.v.dikov@gmail.com>
2023-12-08 17:26:15 +0000

    Finished

--- main.py
+++ main.py
@@ -1,46 +1,82 @@
 import requests
 from discord_webhook import DiscordWebhook, DiscordEmbed
-from deepdives import deepDiveInfo
-from random import randint
 
 r = requests.get("https://drgapi.com/v1/deepdives")
 
 data = r.json()
 variants = data["variants"]
 
-test_list, stage = deepDiveInfo(variants[0])
-
-
-
-
-for s in stage:
-  dd_stage = 'stage ', s['id']
-  dd_primary = 'primary objective:', s['primary']
-  dd_secondary = 'secondary objective:', s['secondary']
-  dd_warnings = "warning:", s['warning']
-  dd_anomaly = 'anomaly:', s['anomaly']
-
-
-test_list, stage = deepDiveInfo(variants[1])
-for s in stage:
-  eed_stage = 'stage ', s['id']
-  eed_primary = 'primary objective:', s['primary']
-  edd_secondary = 'secondary objective:', s['secondary']
-  eed_warning ="warning:", s['warning']
-  edd_warning = 'anomaly:', s['anomaly']
-
-         
-# Add the webhook stuff
 
 url = "https://discord.com/api/webhooks/1181114522203852800/u60JLs9m5QxNPFZzq-1C4maRJPrtpSWuqgY9jtwDbk4Zg6eroap1yObcnwFb4MpYKHFk"
+
 
 webhook = DiscordWebhook(url=url)
 
 embed = DiscordEmbed(
   title="Deep Dive Information", 
-  description="Information on this weeks DRG's Deep Dives, both elite and regular.",
-  color = "13938487"
+  description="<@&1182436710085300245> New Deep Dive just dropped.",
+  color = "d4af37"
 )
+
+dd_stage = variants[0]["stages"]
+
+embed.set_timestamp()
+
+embed.add_embed_field(
+  name=f'''
+  {variants[0]["type"]}:
+  {variants[0]["name"]}
+  {variants[0]["biome"]}''',
+  
+  value=f"""
+  \n
+  > **Stage {variants[0]["stages"][0]["id"]}**
+  > Primary Objective: {dd_stage[0]["primary"]}
+  > Secondary Objective: {dd_stage[0]["secondary"]}
+  > Warning: {dd_stage[0]["warning"]}
+  > Anomaly: {dd_stage[0]["anomaly"]}
+
+  > **Stage {variants[0]["stages"][1]["id"]}**
+  > Primary Objective: {dd_stage[1]["primary"]}
+  > Secondary Objective: {dd_stage[1]["secondary"]}
+  > Warning: {dd_stage[1]["warning"]}
+  > Anomaly: {dd_stage[1]["anomaly"]}
+
+  > **Stage {variants[0]["stages"][2]["id"]}**
+  > Primary Objective: {dd_stage[2]["primary"]}
+  > Secondary Objective: {dd_stage[2]["secondary"]}
+  > Warning: {dd_stage[2]["warning"]}
+  > Anomaly: {dd_stage[2]["anomaly"]}
+  """
+)
+
+edd_stage = variants[1]["stages"]
+
+embed.add_embed_field(
+  name=f'''{variants[1]["type"]}:
+  {variants[1]["name"]}
+  {variants[1]["biome"]}''',
+  
+  value=f"""> **Stage {variants[1]["stages"][0]["id"]}**
+  > Primary Objective: {edd_stage[0]["primary"]}
+  > Secondary Objective: {edd_stage[0]["secondary"]}
+  > Warning: {edd_stage[0]["warning"]}
+  > Anomaly: {edd_stage[0]["anomaly"]}
+
+  > **Stage {variants[1]["stages"][1]["id"]}**
+  > Primary Objective: {edd_stage[1]["primary"]}
+  > Secondary Objective: {edd_stage[1]["secondary"]}
+  > Warning: {edd_stage[1]["warning"]}
+  > Anomaly: {edd_stage[1]["anomaly"]}
+
+  > **Stage {variants[1]["stages"][2]["id"]}**
+  > Primary Objective: {edd_stage[2]["primary"]}
+  > Secondary Objective: {edd_stage[2]["secondary"]}
+  > Warning: {edd_stage[2]["warning"]}
+  > Anomaly: {edd_stage[2]["anomaly"]}"""
+)
+
+
 
 webhook.add_embed(embed)
 
