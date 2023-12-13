0fb2c29de2d5bdb6177525dd3e8c9302f8f26a3c
spderman333 <stefan.v.dikov@gmail.com>
2023-12-13 10:33:55 -0800

    Added comments

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
+# import block
 import requests
 from discord_webhook import DiscordWebhook, DiscordEmbed
+import dateutil.parser as dp
+import os
 
+# Request api
 r = requests.get("https://drgapi.com/v1/deepdives")
 
+# Deal with json
 data = r.json()
 variants = data["variants"]
 
-# Change this out for your webhook url
-# replace (your_role_id_here) with the role id of the role you want to be mentioned when used.
-url = "put_webhook_url_here"
-role_id = "your_role_id_here"
+# Replace "url" with your own webhook url
+url = os.environ['URL_VAL']
+# Replace "role_id" with the id of the role you want mentioned
+role_id = os.environ['ROLE_ID']
 
+# Parse start time stamp
+starttimezulu = data["startTime"]
+starttime = dp.parse(starttimezulu)
+startstamp = starttime.timestamp()
+startstamp = int(startstamp)
 
+# Parse end time stamp
+endtimezulu = data["endTime"]
+endtime = dp.parse(endtimezulu)
+endstamp = endtime.timestamp()
+endstamp = int(endstamp)
+
+# Set webhook url
 webhook = DiscordWebhook(url=url)
 
-
-
+# Set embed main info
 embed = DiscordEmbed(
   title="Deep Dive Information", 
-  description=f"<@&{role_id}> New Deep Dive just dropped.",
+  description=f"""<@&{role_id}> New Deep Dive just dropped.
+  Deep Dive start time: <t:{startstamp}>
+  Deep Dive end time: <t:{endstamp}>
+  """,
   color = "d4af37"
 )
 
-dd_stage = variants[0]["stages"]
-
 embed.set_timestamp()
+
+# Add Deep Dive information
+dd_stage = variants[0]["stages"]
 
 embed.add_embed_field(
   name=f'''
@@ -53,6 +73,7 @@ embed.add_embed_field(
   """
 )
 
+# Add Elite Deep Dive information
 edd_stage = variants[1]["stages"]
 
 embed.add_embed_field(
@@ -60,7 +81,8 @@ embed.add_embed_field(
   {variants[1]["name"]}
   {variants[1]["biome"]}''',
   
-  value=f"""> **Stage {variants[1]["stages"][0]["id"]}**
+  value=f"""
+  > **Stage {variants[1]["stages"][0]["id"]}**
   > Primary Objective: {edd_stage[0]["primary"]}
   > Secondary Objective: {edd_stage[0]["secondary"]}
   > Warning: {edd_stage[0]["warning"]}
@@ -76,10 +98,11 @@ embed.add_embed_field(
   > Primary Objective: {edd_stage[2]["primary"]}
   > Secondary Objective: {edd_stage[2]["secondary"]}
   > Warning: {edd_stage[2]["warning"]}
-  > Anomaly: {edd_stage[2]["anomaly"]}"""
+  > Anomaly: {edd_stage[2]["anomaly"]}
+  """
 )
 
-
+# Add the embed and post the webhook.
 
 webhook.add_embed(embed)
 
